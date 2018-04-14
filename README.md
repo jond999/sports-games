b7e4a1bf36522830570faee42d7f0ee7a6d4d173
jond999 <jondhub999@gmail.com>
2018-04-14 14:55:07 +0100

    draft

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+
 # Sports Games
+
+**Terminal**
+```
+$ node server.js
+```
+**Browser**
+link: [http://localhost:8080](http://localhost:8008)
+
+**Tested**
+> Google Chrome 65.0.3325.181 (Official Build) (64-bit)
---
+++ server.js
@@ -0,0 +1,14 @@
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+	
+	fs.readFile('index.html', function(err, data) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		res.end();
+	});
+})
+
+server.listen(8008);
