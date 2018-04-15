320957d9ae934aaa027601a9d5a8a34714391031
jond999 <jondhub999@gmail.com>
2018-04-15 13:01:19 +0100

    migration to express

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 
-var http = require('http');
+var express = require('express');
 var fs = require('fs');
 
-var server = http.createServer(function (req, res) {
-	
+var port = 8008;
+
+var app = express();
+
+app.get('/', function(req, res) {
 	fs.readFile('index.html', function(err, data) {
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.write(data);
 		res.end();
 	});
-})
+});
 
-server.listen(8008);
+app.listen(port, function() {
+	console.log('The server is running, ' + 
+		'please, open your browser at http://localhost:%s', port);
+});
